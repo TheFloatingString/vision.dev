d1466f3531489bdf9219a278581ba133bfd02d62
Laurence Liang <laurence.w.liang@gmail.com>
2023-06-24 17:56:11 -0400

    WIP POST image to Rest API

--- vision.dev/pages/api/test_helia.js
+++ vision.dev/pages/api/test_helia.js
@@ -10,6 +10,10 @@ export default async function handler(req, res) {
 
     console.log(myImmutableAddress);
 
+    console.log(await s.get(myImmutableAddress));
+
+    console.log(req.body);
+
     res.status(200).json({"data": myImmutableAddress});
 
 }
