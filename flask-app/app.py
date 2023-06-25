6bbb4b7140291e2bee5f2ac27dd057e05bafd808
Steven <gong.steven@hotmail.com>
2023-06-24 23:27:23 -0400

    add yolo inference

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,24 +1,39 @@
 from flask import Flask, render_template
 import os
+from ultralytics import YOLO
+import ipfs_api
+import os
+import cv2
+import numpy as np
+from PIL import Image
 
 app = Flask(__name__)
 
+def predict(img_cid):
+    model = YOLO("yolov8l.pt")  # load a pretrained model (recommended for training)
+    print(f"predicting on image: {img_cid}")
+    ipfs_api.download(img_cid, 'input.png')
+    results = model("input.png")  # predict on an image
 
-# @app.route("/")
-# def home():
-#     return render_template('home.html')
+    # Get the image as a numpy array in BGR format
+    img_bgr = results[0].plot()
+
+    # Convert the image from BGR to RGB
+    img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
+
+    # Convert the numpy array to a PIL image
+    img_pil = Image.fromarray(img_rgb)
+    img_pil.save("output.png")
 
 
-@app.route('/model/<int:node_id>', methods=['POST'])
-def handle_post_request(node_id):
-    # Access the node_id from the URL and the request data
-    # Perform necessary operations
-    # Return a response
 
+@app.route('/predict/<img_cid>', methods=['GET'])
+def handle_post_request(img_cid):
+    print(img_cid)
+    predict(img_cid)
     # Example: Return a JSON response
-    return {'node_id': node_id, 'message': 'POST request received'}
-
+    return {'img_cid': img_cid, 'message': 'POST request received'}
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=6000, debug=True)
